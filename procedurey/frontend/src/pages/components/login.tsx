049db751fb2029e8af128683baef1265f51c6176
AIN1995 <abe19950311@gmail.com>
2022-09-21 22:35:10 +0900

    0921top画面作成中

---
+++ procedurey/frontend/src/pages/components/login.tsx
@@ -0,0 +1,12 @@
+
+
+
+const Testlink = ()=>{
+    return (
+        <div>
+            <h1>test</h1>
+        </div>
+    )
+}
+
+export default Testlink;
---
+++ procedurey/frontend/src/pages/components/toppage.tsx
@@ -0,0 +1,70 @@
+import Image from 'next/image'
+import styled from "styled-components"
+import Link from "next/link"
+
+export const TopPage = ()=>{
+
+    const Body = styled.div`
+    margin: 0px;
+    padding: 0px;
+    `
+
+    const Main = styled.div`
+
+
+    h1 {
+        position:absolute;
+        top: 40%;
+	left: 50%;
+	-ms-transform: translate(-50%,-50%);
+	-webkit-transform: translate(-50%,-50%);
+	transform: translate(-50%,-50%);
+	margin:0;
+	padding:0;
+    }
+
+    h3 {
+        position:absolute;
+        top: 40%;
+	left: 50%;
+	-ms-transform: translate(-50%,-50%);
+	-webkit-transform: translate(-50%,-50%);
+	transform: translate(-50%,-50%);
+	margin-top:40px;
+	padding:0;
+    }
+    `
+
+    const Button = styled.button`
+        position:absolute;
+        top: 50%;
+	left: 50%;
+	-ms-transform: translate(-50%,-50%);
+	-webkit-transform: translate(-50%,-50%);
+	transform: translate(-50%,-50%);
+	margin:0;
+	padding:0;
+    `
+
+
+
+    return (
+        
+        <div>
+        <Body>
+    
+            <Main>
+            <div>
+                <Image src="/main.png" layout="fill" alt="logo"/>   
+                <h1>Procedurey</h1>
+                <h3>様々な手続きが、一目で分かる</h3>
+                <Link href="/components/login">
+                    <Button><button>利用する</button></Button>
+                </Link>
+            </div>
+            </Main>
+
+        </Body>
+        </div>
+    )
+}
