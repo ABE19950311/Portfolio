505714791a3dca0de4dcce8403634c8d4b6c4035
AIN1995 <abe19950311@gmail.com>
2022-10-01 21:53:21 +0900

    1001Fullcalendarで表示まで確認

--- procedurey/frontend/next.config.js
+++ procedurey/frontend/next.config.js
@@ -1,22 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
-  require("next-transpile-modules")([
-    "@fullcalendar/common",
-    "@fullcalendar/common",
-    "@fullcalendar/daygrid",
-    "@fullcalendar/interaction",
-    "@fullcalendar/react",
-    "@fullcalendar/timegrid",
-  ]);
-}
 
-/** @type {import('next').NextConfig} */
 const withTM = require("next-transpile-modules")([
-  "@fullcalendar/common",
   "@fullcalendar/common",
   "@fullcalendar/daygrid",
   "@fullcalendar/interaction",
@@ -24,8 +7,12 @@ const withTM = require("next-transpile-modules")([
   "@fullcalendar/timegrid",
 ]);
 
+/** @type {import('next').NextConfig} */
 module.exports = withTM({
-  // your custom config goes here
+    reactStrictMode: true,
+    compiler: {
+      styledComponents: true,
+    },
 });
 
-module.exports = nextConfig
+
