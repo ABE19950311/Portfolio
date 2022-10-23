2e5625fa4dfbf4a7037212b08d3addd3e4c7cb74
AIN1995 <abe19950311@gmail.com>
2022-10-23 18:34:47 +0900

    　

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::API
 
     def set_csrf_token
         cookies['CSRF-TOKEN'] = {
-            domain: 'amazonaws.com' #親ドメイン
+            domain: 'amazonaws.com',
             value: form_authenticity_token
         }
     end
