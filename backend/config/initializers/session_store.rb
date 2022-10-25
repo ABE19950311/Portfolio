b9aad459cba848cacc39d694e8576ea36e4b5311
AIN1995 <abe19950311@gmail.com>
2022-10-25 22:18:53 +0900

    　

--- backend/config/initializers/session_store.rb
+++ backend/config/initializers/session_store.rb
@@ -3,3 +3,7 @@ if Rails.env === 'production'
 else
     Rails.application.config.session_store :cookie_store, key: '_auth-app-api'
 end
+
+Rails.application.configure do
+    config.action_dispatch.cookies_same_site_protection = nil
+end
