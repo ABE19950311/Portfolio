ef1a7ed30e199e7b213f6c041edb6a95fb33864d
AIN1995 <abe19950311@gmail.com>
2022-10-27 20:19:08 +0900

    test修正

--- frontend/__tests__/toppage.spec.tsx
+++ frontend/__tests__/toppage.spec.tsx
@@ -4,7 +4,7 @@ import { render,screen,RenderResult } from "@testing-library/react";
 describe("Sampleコンポーネント", () => {
     test("should first", () => {
         const { getByText } = render(<Home />);
-        expect(getByText("Procedurey")).toBeTruthy();
+        expect(getByText("Dailylifey")).toBeTruthy();
         expect(getByText("様々な手続きが、一目で分かる")).toBeTruthy();
         expect(getByText("利用する")).toBeTruthy();
     });
