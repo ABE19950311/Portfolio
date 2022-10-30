ecd06f606738c1cfa1be2855738c9b16617e06db
AIN1995 <abe19950311@gmail.com>
2022-10-30 22:58:30 +0900

    　

--- backend/spec/factories/boards.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :board do
-    postdate { "2022-10-28" }
-    posttitle { "MyString" }
-    username { "MyString" }
-  end
-end
--- backend/spec/factories/user_boards.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :user_board do
-    
-  end
-end
