50ba52634c1086c9f103cee18b1e828de66b22d1
AIN1995 <abe19950311@gmail.com>
2022-12-06 20:57:49 +0900

    　

--- frontend/src/components/fetchdata.tsx
+++ frontend/src/components/fetchdata.tsx
@@ -8,11 +8,6 @@ export const FetchData = ()=>{
     const [userid,setUserid] = useState("")
     const [loginstate,setLoginstate] = useState("")
 
-    console.log(process.env.PRODUCTION_ADDRESS)
-    console.log(process.env.NEXT_PUBLIC_PRODUCTION_ADDRESS)
-    console.log(process.env.TEST)
-    console.log(process.env.NEXT_PUBLIC_TEST)
-
     useEffect(()=>{
         if(process.env.NEXT_PUBLIC_ADDRESS!==undefined) {
             setEnv(process.env.NEXT_PUBLIC_ADDRESS as string)
@@ -25,9 +20,8 @@ export const FetchData = ()=>{
                 setError(error)
             })
         }else{
-            if(!process.env.PRODUCTION_ADDRESS) return
-            setEnv(process.env.PRODUCTION_ADDRESS)
-            axios.get(process.env.PRODUCTION_ADDRESS+"/sessionid")
+            setEnv("https://backend.dailylifey.com")
+            axios.get("https://backend.dailylifey.com/sessionid")
             .then(res=>{
                 axios.defaults.headers.common['X-CSRF-Token'] = res.headers['x-csrf-token'];
                 setUserid(res.data.id)
